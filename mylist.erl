ef67dc3d5c7d56260da75a86b400f5589a3c9815
Fei Du <fei.du@example.com>
2015-07-11 18:59:23 +0800

    Move functions in file_1 to mylist and delete file_1
    file_1.erl deleted
    mylist.erl added functions in file_1.erl

--- mylist.erl
+++ mylist.erl
@@ -5,3 +5,14 @@
 
 sayHi() -> 
     io:format("Hello world!~n").
+
+range(X, Y) ->
+    lists:seq(X, Y).
+
+range(X, Y, Incr) -> 
+    lists:seq(X, Y, Incr).
+
+range2(X, Y, Incr) ->
+    List1 = lists:seq(X, Y),
+    List2 = [Z || Z <- List1, Z rem Incr =:= 1],
+    List2.
