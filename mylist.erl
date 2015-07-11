edf1f133ee83213ee8952d793491397fa1511f7d
Fei Du <fei.du@example.com>
2015-07-11 19:51:56 +0800

    Add function cut in mylist.erl
    cut/3 cut a list from one index to another index

--- mylist.erl
+++ mylist.erl
@@ -16,3 +16,18 @@ range2(X, Y, Incr) ->
     List1 = lists:seq(X, Y),
     List2 = [Z || Z <- List1, Z rem Incr =:= 1],
     List2.
+
+cut([], _, _) -> 
+    [];
+cut(List, From, To) ->
+    List0 = cut(List, From, To, []),
+    List1 = lists:reverse(List0),
+    List1.
+
+cut([H|T], From, To, Result) -> 
+    case {From,To} of
+        {_, 0} -> Result;
+        {0, _} -> cut(T, From, To-1, [H | Result]);
+        {_, _} -> cut(T, From-1, To-1, Result)
+    end.
+
